7a6f8a6db11dfc9a558ebad67cbdadfa5ac3326d
Łukasz Matysiak <december0123@gmail.com>
2016-04-16 21:32:22 +0200

    Add third class to prototype

--- src/prototype.py
+++ src/prototype.py
@@ -18,21 +18,26 @@ for i in range(300):
     X.append([i, i])
     y.append(1)
 
-# "f(x) != x"
-for i in range(150):
+# "f(x) > x"
+for i in range(300):
     X.append([i, i+random.randint(1, 100)])
-    y.append(0)
+    y.append(2)
+
+# "f(x) < x"
+for i in range(300):
     X.append([i, i-random.randint(1, 100)])
-    y.append(0)
+    y.append(3)
 
 clf = MLPClassifier(algorithm='l-bfgs', alpha=1e-5, hidden_layer_sizes=(1000), random_state=1)
 classifier = clf.fit(X, y)
 
 test_1 = [[i, i] for i in range(1000)]
-test_0 = [[i, i+random.randint(1, 200)] for i in range(200)]
+test_2 = [[i, i+random.randint(1, 200)] for i in range(1000)]
+test_3 = [[i, i-random.randint(1, 200)] for i in range(1000)]
 
 check_results(test_1, classifier.predict(test_1), 1)
-check_results(test_0, classifier.predict(test_0), 0)
+check_results(test_2, classifier.predict(test_2), 2)
+check_results(test_3, classifier.predict(test_3), 3)
 
 print("Done.")
 
