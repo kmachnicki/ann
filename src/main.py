7177e7333f263e71ac205393df76445a899a0edd
kmachnicki <kamil0602@gmail.com>
2016-04-17 18:40:41 +0200

    Corrected path

--- src/main.py
+++ src/main.py
@@ -2,11 +2,12 @@
 
 from src.experiments import run_experiment
 from src.dataset import DataSet
+from src.consts import INPUT_DATA_FILE
 
 
 def main():
     ds = DataSet()
-    with open("../stopien_zlosliwosci.csv", "r", newline='', encoding="utf8") as csv_file:
+    with open(INPUT_DATA_FILE, "r", newline='', encoding="utf8") as csv_file:
         ds.extract_from_csv(csv_file)
     for layer_size in range(50, 550, 100):
         print("Running experiment for layer_size =", layer_size)
