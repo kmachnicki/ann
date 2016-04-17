c1a29906b86b4ec9ca935dc4b3dff6ddc1f5a2a1
Łukasz Matysiak <december0123@gmail.com>
2016-04-17 18:57:12 +0200

    Update comment

--- src/experiments.py
+++ src/experiments.py
@@ -10,7 +10,12 @@ def run_experiment(X_train, y, algorithm='l-bfgs', max_iter=100, alpha=1e-6, hid
     :param y: Classes
     :param algorithm:
     :param max_iter:
-    :param alpha: Learning rate
+    :param alpha: parameter for regularization term, aka penalty term,
+            that combats overfitting by constraining the size of the weights.
+            Increasing alpha may fix high variance (a sign of overfitting) by encouraging smaller weights,
+            resulting in a decision boundary plot that appears with lesser curvatures.
+            Similarly, decreasing alpha may fix high bias (a sign of underfitting) by encouraging larger weights,
+            potentially resulting in a more complicated decision boundary.
     :param hidden_layer_sizes:
     :param cv: kfold crossvalidation, k = 10
     :param n_jobs:
