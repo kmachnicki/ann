1221c788be11e1f1e2e5c821859ed4faed2ef1cb
Łukasz Matysiak <december0123@gmail.com>
2016-04-19 20:19:30 +0200

    Experiments

--- src/experiments.py
+++ src/experiments.py
@@ -5,16 +5,41 @@ from sklearn.model_selection import cross_val_score
 from sklearn.feature_selection import SelectKBest
 
 
-def select_features(X_train, y_train, k_best_features=10):
+def select_features(X_train, y_train, k_best_features='all'):
     return SelectKBest(k=k_best_features).fit_transform(X_train, y_train)
 
 
 def run_experiments_for_five_numbers_of_neurons(X_train, y, algorithm='l-bfgs', max_iter=100, alpha=1e-6,
-                                                cv=10, n_jobs=-1):
-    for layer_size in range(50, 550, 100):
-        print("Running experiment for layer_size =", layer_size)
-        run_experiment(X_train, y, algorithm=algorithm, max_iter=max_iter, alpha=alpha,
-                       hidden_layer_size=layer_size, cv=cv, n_jobs=n_jobs)
+                                                n_features='all', cv=10, n_jobs=-1,
+                                                min_layer_size=None, max_layer_size=None):
+    print("Running experiments.")
+    print("Parameters: algorithm={}, max_iter={}, alpha={}, n_features={}, cv={}".format(algorithm, max_iter, alpha,
+                                                                                         n_features, cv))
+    X_selected = select_features(X_train, y, n_features)
+    num_of_selected_features = len(X_selected[0])
+    if num_of_selected_features == len(X_train[0]):
+        print("Using all features.")
+    else:
+        print("Using {} selected features.".format(n_features))
+
+    if min_layer_size and not max_layer_size:
+        max_layer_size = min_layer_size * 5
+    elif not min_layer_size and not max_layer_size:
+        min_layer_size = num_of_selected_features
+        max_layer_size = min_layer_size * 5
+
+    best = (0, 0, 0)
+    for layer_size in range(min_layer_size, max_layer_size,
+                            int((max_layer_size - min_layer_size) / 5)):
+        print("layer_size =", layer_size)
+        scores = run_experiment(X_selected, y, algorithm=algorithm, max_iter=max_iter, alpha=alpha,
+                                hidden_layer_size=layer_size, cv=cv, n_jobs=n_jobs)
+
+        if scores.mean() - scores.std() * 2 > best[0]:
+            best = (scores.mean(), scores.std() * 2, layer_size)
+        print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+        print()
+    print("Best: ", best)
 
 
 def run_experiment(X_train, y, algorithm='l-bfgs', max_iter=100, alpha=1e-6, hidden_layer_size=100, cv=10, n_jobs=-1):
@@ -37,4 +62,4 @@ def run_experiment(X_train, y, algorithm='l-bfgs', max_iter=100, alpha=1e-6, hid
 
     clf = MLPClassifier(algorithm=algorithm, max_iter=max_iter, alpha=alpha, hidden_layer_sizes=hidden_layer_size,
                         random_state=1)
-    print(cross_val_score(clf, X_train, y, cv=cv, n_jobs=n_jobs))
+    return cross_val_score(clf, X_train, y, cv=cv, n_jobs=n_jobs)
--- src/main.py
+++ src/main.py
@@ -1,24 +1,16 @@
 #!/usr/bin/python
 
-from src.experiments import run_experiments_for_five_numbers_of_neurons, select_features
+from src.experiments import run_experiments_for_five_numbers_of_neurons
 from src.dataset import DataSet
 from src.consts import INPUT_DATA_FILE
-
-k_best_features = 10
 
 
 def main():
     ds = DataSet()
     with open(INPUT_DATA_FILE, "r", newline='', encoding="utf8") as csv_file:
         ds.extract_from_csv(csv_file)
-    print("Running experiments with all features")
-    run_experiments_for_five_numbers_of_neurons(ds.X, ds.y, algorithm='l-bfgs', max_iter=100,
-                                                alpha=1e-5)
-    X_selected = select_features(ds.X, ds.y, k_best_features)
-    print("Running experiments with selected features")
-    run_experiments_for_five_numbers_of_neurons(X_selected, ds.y, algorithm='l-bfgs', max_iter=100,
-                                                alpha=1e-5)
-
+    run_experiments_for_five_numbers_of_neurons(ds.X, ds.y, algorithm='l-bfgs', max_iter=150,
+                                                alpha=1e-5, n_features=2, min_layer_size=100)
 
 if __name__ == '__main__':
     main()
