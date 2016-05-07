515d5d4e2d15af23174435033a479aa08a5b1e7f
Łukasz Matysiak <december0123@gmail.com>
2016-05-07 18:31:25 +0200

    Count how often features were selected

--- src/experiments.py
+++ src/experiments.py
@@ -5,10 +5,11 @@ from sklearn.feature_selection import SelectKBest
 from src.consts import RANDOM_STATE
 from sklearn.model_selection import KFold
 import numpy as np
+from collections import Counter
 
 
 def get_selected_features_indices(X, y, k_best_features='all'):
-    return SelectKBest(k=k_best_features).fit(X, y).get_support()
+    return SelectKBest(k=k_best_features).fit(X, y).get_support(indices=True)
 
 
 # sgd = stochastic gradient descent
@@ -17,14 +18,16 @@ def run_experiment(X, y, algorithm='sgd', max_iter=100, alpha=1e-6,
     X = np.array(X)
     y = np.array(y)
     mean = 0
+    counter = Counter()
     for train_set, test_set in KFold(n_folds=kfold).split(X, y):
         X_train, X_test, y_train, y_test = X[train_set], X[test_set], y[train_set], y[test_set]
         indices = get_selected_features_indices(X_train, y_train, k_best_features=n_features)
+        counter.update(indices)
         X_train = [row[indices] for row in X_train]
         X_test = [row[indices] for row in X_test]
         mean += calculate_score(X_train, y_train, X_test, y_test, algorithm=algorithm, max_iter=max_iter, alpha=alpha,
                                 learning_rate=learning_rate, hidden_layer_size=hidden_layer_size)
-    return mean / kfold
+    return mean / kfold, counter
 
 
 def calculate_score(X_train, y_train, x_test, y_test, algorithm='sgd', max_iter=200, alpha=1e-6, hidden_layer_size=100,
--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ from src.experiments import run_experiment
 from src.dataset import DataSet
 from src.consts import INPUT_DATA_FILE
 import matplotlib.pyplot as plt
+from collections import Counter
 
 
 def main():
@@ -13,14 +14,18 @@ def main():
 
     print("Ranking (descending)", ds.create_features_ranking(use_names=True))
     results = {}
+    final_counter = Counter()
     for n_features in range(1, ds.number_of_features, 1):
-        results[n_features] = run_experiment(ds.X, ds.y, algorithm='sgd', max_iter=1000,
+        results[n_features], counter = run_experiment(ds.X, ds.y, algorithm='sgd', max_iter=1000,
                                              alpha=1e-6, learning_rate='constant',
                                              n_features=n_features, hidden_layer_size=n_features)
+        final_counter.update(counter)
     y = []
+
     for n_features, score in sorted(results.items()):
         print("Score for {} features = {}".format(n_features, score))
         y.append(score)
+    print(final_counter)
 
     x = range(1, ds.number_of_features, 1)
     plt.plot(x, y)
