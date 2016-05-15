a067416d56cad4666db4158a902a9c70e3891d03
kmachnicki <kamil0602@gmail.com>
2016-05-15 15:28:07 +0200

    Added Python-ELM to install script

--- install.sh
+++ install.sh
@@ -4,4 +4,12 @@ echo "***** Installing required Python packages *****"
 
 pip3 install -r requirements.txt
 
+echo "***** Downloading required Python-ELM module *****"
+
+if ! [ -d "modules" ] ; then
+	mkdir "modules"
+fi
+
+git clone https://github.com/kmachnicki/Python-ELM modules/Python-ELM
+
 echo "***** All done, enjoy *****"
